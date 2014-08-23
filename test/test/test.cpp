ba28a87c6f86abd7c951364713f88ea9b4bdddf9
zjc <z_jc@foxmail.com>
2014-08-23 23:42:31 +0800

    上位机主程序添加模块push和circle_pick
    Signed-off-by: zjc <z_jc@foxmail.com>

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -4,6 +4,13 @@
 #include <fstream>
 using namespace std;
 #include "MySerialPort.h"
+#define ACTION_QUEUE_NUM 5 //20个动作一组
+void push(char receive);
+
+char temp[ACTION_QUEUE_NUM][50];//最近动作序列
+int action_group = 0;
+int action_num = 0;
+ofstream file("output.txt");//打开文件
 
 
 int main(int argc,char *argv[])
@@ -18,7 +25,6 @@ int main(int argc,char *argv[])
 	int val = 0;
 	char receive = NULL;
 
-	ofstream file("output.txt");//打开文件
 	if(!file)return 0;
 
 	while (true)
@@ -33,31 +39,54 @@ int main(int argc,char *argv[])
 				obj.ReadData(&receive,1);
 
 				cout << receive;
-				file << receive ;
+				//file << receive ;
 				//换行
 				if(receive == ' '){
-					file << "\n";
+				//	file << "\n";
 					cout<<endl;
 				}
+				push(receive);
 					
 				Sleep(2);
 			}
 		}
 
 
-		else
-		{
+		else{
 			cout << "SerialPort is not opened !" << endl;
 		}
-
-		if ( val == 726)
-		{
+		if ( val == 726){
 			break;
 		}
 	}
 	
 
 	obj.Close();
-	
 	return 0;
+}
+
+//循环二维数组，返回正序二维数组
+char* circle_pick(char** temp,int length,int begin){
+	return 0;
+}
+
+//不断循环添加到temp数组中
+void push(char receive){
+	if(action_group < ACTION_QUEUE_NUM){
+					temp[action_group][action_num] = receive;
+					action_num++;
+					if(receive == ' '){
+						temp[action_group][action_num] = '\0';
+						action_group ++;
+						action_num = 0;
+					}
+				}else{
+					int i = 0;
+					while(i<ACTION_QUEUE_NUM){
+						cout<<temp[i]<<endl;
+						file<<temp[i]<<endl;
+						i++;
+					}
+					action_group =0;
+				}
 }
---
+++ 姿态预测/output_splict.m
@@ -0,0 +1,7 @@
+ax = output(:,1);
+ay = output(:,2);
+az = output(:,3);
+gx = output(:,4);
+gy = output(:,5);
+gz = output(:,6);
+
